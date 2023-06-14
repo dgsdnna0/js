0338ab07eaaf218265b97102526960639bb17938
dgsdnna0 <136586998+dgsdnna0@users.noreply.github.com>
2023-06-14 17:45:55 +0700

    Create js.js

---
+++ js.js
@@ -0,0 +1,25 @@
+// Example JavaScript code
+// Add your game-specific JavaScript code here
+
+// Function to initialize the game
+function initGame() {
+    // Game initialization code goes here
+    console.log("Game initialized");
+}
+
+// Function to start the game
+function startGame() {
+    // Game start code goes here
+    console.log("Game started");
+}
+
+// Function to end the game
+function endGame() {
+    // Game end code goes here
+    console.log("Game ended");
+}
+
+// Call the initGame function when the DOM is ready
+document.addEventListener("DOMContentLoaded", function() {
+    initGame();
+});
